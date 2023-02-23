c739cc04f508961f41bea6713ae4537a65a798c6
scumatteo <matteo.scucchia@studio.unibo.it>
2023-02-23 20:13:23 +0100

    changed visualizer

--- RegionLearner/src/visualizer.py
+++ RegionLearner/src/visualizer.py
@@ -35,7 +35,6 @@ def parse_parameters():
     parser.add_argument('--dataset-file', type=str)
     parser.add_argument('--centroids-file', type=str)
     parser.add_argument('--graph-file', type=str)
-    
     parser.add_argument('--position-color', type=str)
     parser.add_argument('--position-dim', type=int)
     parser.add_argument('--position-annotation', type=str)
@@ -64,6 +63,8 @@ def parse_parameters():
         position_annotation = config["visualizer"]["position_annotation"] if "position_annotation" in config["visualizer"] else position_annotation
         circle_dim = int(config["visualizer"]["circle_dim"]) if "circle_dim" in config["visualizer"] else circle_dim 
         annotate_regions = config["visualizer"]["annotate_regions"] == "true" if "annotate_regions" in config["visualizer"] else annotate_regions 
+        x_label = config["visualizer"]["x_label"] == "true" if "x_label" in config["visualizer"] else x_label 
+        y_label = config["visualizer"]["y_label"] == "true" if "y_label" in config["visualizer"] else y_label 
 
     path_to_dataset = args.path_to_dataset if args.path_to_dataset else path_to_dataset
     images_folder = args.images_folder if args.images_folder else images_folder
